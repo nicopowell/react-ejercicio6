47a260e0f2f0d4b464dda75f957c7462eba104e6
Nicolas Powell <nicopowell30@gmail.com>
2023-05-29 22:57:25 -0300

    agregué la funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import FormularioColor from './components/FormularioColor';
+
 
 function App() {
   return (
-    <>
-    </>
+    <Container className="my-5 mainPage">
+        <FormularioColor></FormularioColor>
+      </Container>
   )
 }
 
